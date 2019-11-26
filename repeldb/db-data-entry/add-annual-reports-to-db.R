3ed3b9607e91f8258b88c601f5c1a2d8de8eddc1
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2019-11-26 15:15:26 -0500

    script and folder to read and save data processed in wahis/inst/process_annual_reports.R

---
+++ repeldb/db-data-entry/add-annual-reports-to-db.R
@@ -0,0 +1,40 @@
+# this is run one time to populate the DB, from here is it updated in the pipeline
+
+setwd(here::here("repeldb"))
+suppressMessages(suppressWarnings(suppressPackageStartupMessages({
+  library(tidyverse)
+  library(xml2)
+  library(rvest)
+  library(stringi)
+  library(RPostgres)
+  library(scrapetools)
+  library(DBI)
+  library(assertthat)
+  library(arkdb)
+})))
+
+# DB connect
+base::readRenviron(".env")
+conn <- dbConnect(
+  RPostgres::Postgres(),
+  host = Sys.getenv("DEPLOYMENT_SERVER_URL"),
+  port = Sys.getenv("POSTGRES_EXTERNAL_PORT"),
+  user = Sys.getenv("POSTGRES_USER"),
+  password = Sys.getenv("POSTGRES_PASSWORD"),
+  dbname = Sys.getenv("POSTGRES_DB")
+)
+
+# Get data from AWS
+
+# Script to sync data files
+# See https://ecohealthalliance.github.io/eha-ma-handbook/11-cloud-computing-services.html
+# For credentials setup
+
+wahis:::pull_aws(bucket = "wahis-data", object = "data-processed.tar.xz", dir = "db-data-entry") # pushed up in wahis/inst/process_annual_reports.r
+
+# Add to db
+files <- fs::dir_ls(path = "db-data-entry/data-processed/db", regexp = "annual_reports")
+arkdb::unark(files, db_con = conn, overwrite = TRUE,
+             streamable_table = streamable_readr_csv(), lines = 50000L, col_types = cols(.default = col_character()))
+
+
