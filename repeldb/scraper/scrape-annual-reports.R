48dfde3e3fb0a4501c65b383de3befe627693470
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2019-11-26 18:04:54 -0500

    maintain ingest log to track annual report ingest errors

--- repeldb/scraper/scrape-annual-reports.R
+++ repeldb/scraper/scrape-annual-reports.R
@@ -52,15 +52,18 @@ annual_reports_status <- tbl(conn, "annual_reports_status") %>%
   collect() %>%
   rename(report_year = year) %>%
   mutate(report_year = as.character(report_year))
-annual_reports_metadata <- tbl(conn, "annual_reports_metadata")  %>%
-  distinct(country, report_year, report_months) %>%
-  collect() %>%
-  mutate(semester = recode(report_months, "Jan-Dec" = "semester0", "Jan-Jun" = "semester1", "Jul-Dec" = "semester2")) %>%
-  arrange(country, report_year, semester) %>%
-  select(-report_months) %>%
-  mutate(in_database = TRUE)
 
-reports_to_get <- left_join(annual_reports_status, annual_reports_metadata, by = c("country", "report_year", "semester")) %>%
+ingest_log <- dbReadTable(conn, "annual_reports_ingest_status_log") %>% mutate(in_database = as.logical(in_database))
+
+# annual_reports_metadata <- tbl(conn, "annual_reports_metadata")  %>%
+#   distinct(country, report_year, report_months) %>%
+#   collect() %>%
+#   mutate(semester = recode(report_months, "Jan-Dec" = "semester0", "Jan-Jun" = "semester1", "Jul-Dec" = "semester2")) %>%
+#   arrange(country, report_year, semester) %>%
+#   select(-report_months) %>%
+#   mutate(in_database = TRUE)
+
+reports_to_get <- left_join(annual_reports_status, ingest_log, by = c("code", "report_year", "semester")) %>%
   mutate(in_database = coalesce(in_database, FALSE)) %>%
   filter(reported & !in_database) %>%
   mutate(semester = substr(semester, nchar(semester), nchar(semester))) %>%
@@ -69,9 +72,12 @@ reports_to_get <- left_join(annual_reports_status, annual_reports_metadata, by =
 #  sample_n(10) %>% # selecting a random sample is useful for testing
   sample_frac(1)
 
+
+# update annual report status with existing errors
+
+
 # Pulling reports ----------------------------
 message("Pulling ", nrow(reports_to_get), " reports")
-
 
 report_resps <- map_curl(
   urls = reports_to_get$url,
@@ -83,8 +89,17 @@ report_resps <- map_curl(
 )
 
 report_resps <- map_if(report_resps, is.null,
-                          function(x) list(report_status = "failed to fetch"))
+                          function(x) list(ingest_status = "failed to fetch"))
 
+
+# Update ingest log -------------------------------------------------------
+
+ingest_status_log <- reports_to_get %>%
+  select(code, report_year, semester) %>%
+  mutate(ingest_status = map_chr(report_resps, ~.x$ingest_status)) %>%
+  mutate(in_database = ingest_status == "available") %>%
+  mutate(ingest_error = ifelse(!in_database, ingest_status, NA)) %>%
+  select(-ingest_status)
 
 
 # Updating databae  ----------------------------
@@ -110,7 +125,7 @@ reports_to_update <- reports_to_get %>%
   mutate(year = report_year) %>%
   select(colnames(tbl(conn, "annual_reports_status"))) %>%
   mutate(datetime_fetched = Sys.time(),
-         fetched_status = map_chr(report_resps, "report_status"))
+         fetched_status = map_chr(report_resps, "ingest_status"))
 
 removed <- update_sql_table(conn, "annual_reports_status", reports_to_update, c("country", "code", "year", "semester"))
 
@@ -118,7 +133,10 @@ annual_report_tables <- wahis::transform_annual_reports(report_resps) %>%
   keep(~nrow(.) > 0) # This could probably be handled inside transform_annual_reports
 
 iwalk(annual_report_tables,
-      ~update_sql_table(conn, paste0("annual_reports_", .y), .x,
+      ~update_sql_table(conn,  .y, .x,
                         c("country", "report_year", "report_months"))
 )
+
+# update ingest log
+update_sql_table(conn, "annual_reports_ingest_status_log", ingest_status_log, c("code", "report_year", "semester"))
 
