1c35577cb19b65da8033761bb7b595fccb2e7baf
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2019-12-03 11:10:22 -0500

    separate library and functions

--- repeldb/db-data-entry/add-reports-to-db.R
+++ repeldb/db-data-entry/add-reports-to-db.R
@@ -1,27 +1,10 @@
 # this is run one time to populate the DB, from here is it updated in the pipeline
 
-suppressMessages(suppressWarnings(suppressPackageStartupMessages({
-  library(tidyverse)
-  library(xml2)
-  library(rvest)
-  library(stringi)
-  library(RPostgres)
-  library(scrapetools)
-  library(DBI)
-  library(assertthat)
-  library(arkdb)
-})))
+source(here::here("repeldb", "packages.R"))
+source(here::here("repeldb", "functions.R"))
 
 # DB connect
-base::readRenviron(here::here("repeldb", ".env"))
-conn <- dbConnect(
-  RPostgres::Postgres(),
-  host = Sys.getenv("DEPLOYMENT_SERVER_URL"),
-  port = Sys.getenv("POSTGRES_EXTERNAL_PORT"),
-  user = Sys.getenv("POSTGRES_USER"),
-  password = Sys.getenv("POSTGRES_PASSWORD"),
-  dbname = Sys.getenv("POSTGRES_DB")
-)
+conn <- wahis_db_connect()
 
 # Get data from AWS
 
@@ -40,3 +23,5 @@ arkdb::unark(files, db_con = conn, overwrite = TRUE,
 files <- fs::dir_ls(path = here::here("repeldb/db-data-entry/data-processed/db"), regexp = "outbreak_reports")
 arkdb::unark(files, db_con = conn, overwrite = TRUE,
              streamable_table = streamable_readr_csv(), lines = 50000L, col_types = cols(.default = col_character()))
+
+dbDisconnect(conn)
--- repeldb/scraper/scrape-annual-reports.R
+++ repeldb/scraper/scrape-annual-reports.R
@@ -2,48 +2,12 @@
 
 # A script get the list of available annual reports on WAHIS
 
-
-suppressMessages(suppressWarnings(suppressPackageStartupMessages({
-  library(tidyverse)
-  library(xml2)
-  library(rvest)
-  library(stringi)
-  library(RPostgres)
-  library(scrapetools)
-  library(DBI)
-  library(assertthat)
-  library(wahis)
-  library(glue)
-})))
-
+source(here::here("repeldb", "packages.R"))
+source(here::here("repeldb", "functions.R"))
 
 # Connect to database ----------------------------
 message("Connect to database")
-
-# This block is nice for allowsing both interactive and deployment use
-if ((Sys.getenv("REPEL_TEST") == "1") || (interactive() && Sys.getenv("RSTUDIO") == "1")) {
-  base::readRenviron(here::here("repeldb", ".env"))
-  conn <- dbConnect(
-    RPostgres::Postgres(),
-    host = Sys.getenv("DEPLOYMENT_SERVER_URL"),
-    port = Sys.getenv("POSTGRES_EXTERNAL_PORT"),
-    user = Sys.getenv("POSTGRES_USER"),
-    password = Sys.getenv("POSTGRES_PASSWORD"),
-    dbname = Sys.getenv("POSTGRES_DB")
-  )
-  if (require("connections")) {
-    connections::connection_view(conn, name = "repel", connection_code = "repel")
-  }
-} else {
-  conn <- dbConnect(
-    RPostgres::Postgres(),
-    host = Sys.getenv("POSTGRES_HOST"),
-    port = Sys.getenv("POSTGRES_PORT"),
-    user = Sys.getenv("POSTGRES_USER"),
-    password = Sys.getenv("POSTGRES_PASSWORD"),
-    dbname = Sys.getenv("POSTGRES_DB")
-  )
-}
+conn <- wahis_db_connect()
 
 # Finding unfetched reports in database ----------------------------
 message("Finding unfetched reports in database")
@@ -58,8 +22,7 @@ ingest_log <- dbReadTable(conn, "annual_reports_ingest_status_log") %>% mutate(i
 reports_to_get <- left_join(annual_reports_status, ingest_log, by = c("code", "report_year", "semester")) %>%
   mutate(in_database = coalesce(in_database, FALSE)) %>%
   filter(reported & !in_database) %>%
-  mutate(url = glue("https://www.oie.int/wahis_2/public/wahid.php/Reviewreport/semestrial/review?year={report_year}&semester={semester}&wild=0&country={code}&this_country_code={code}&detailed=1"),
-       filename = glue("{country}_{report_year}_sem{semester}.html")) %>%
+  mutate(url = glue("https://www.oie.int/wahis_2/public/wahid.php/Reviewreport/semestrial/review?year={report_year}&semester={semester}&wild=0&country={code}&this_country_code={code}&detailed=1")) %>%
   sample_frac(1)
 
 # Pulling reports ----------------------------
@@ -88,24 +51,8 @@ ingest_status_log <- reports_to_get %>%
   select(-ingest_status)
 
 
-# Updating databae  ----------------------------
+# Updating database  ----------------------------
 message("Updating database")
-
-# This replaces the rows in the database table with the ones in `updates`,
-# matching on `id_fields`. The replacements are just appended on the end.
-# There might be a better way using SQL UPDATE.  It returns the fields that were
-# removed.
-#TODO: This works with characters, but change so it works with other types
-update_sql_table <- function(conn, table, updates, id_fields, verbose = TRUE) {
-  sql_table <- tbl(conn, table)
-  assert_that(identical(sort(colnames(sql_table)), sort(colnames(updates))))
-  criteria <- distinct(select(updates, id_fields))
-  selector <- paste0("(", do.call(paste, c(imap(criteria, ~paste0("", .y, " = \'", .x, "\'")), sep = " AND ")), ")", collapse = " OR ")
-  removed <- DBI::dbGetQuery(conn, glue("DELETE FROM {table} WHERE {selector} RETURNING * ;"))
-  dbAppendTable(conn, table, updates)
-  if (verbose) message("Replacing ", nrow(removed), " old records with ", nrow(updates), " new records")
-  return(removed)
-}
 
 # tables
 annual_report_tables <- wahis::transform_annual_reports(report_resps) %>%
@@ -116,24 +63,8 @@ iwalk(annual_report_tables,
                         c("country_iso3c", "report_year", "report_months"))
 )
 
-# update ingest log
+# ingest log
 update_sql_table(conn, "annual_reports_ingest_status_log", ingest_status_log, c("code", "report_year", "semester"))
 
 dbDisconnect(conn)
 message("Done updating annual reports.")
-
-
-# status
-# reports_to_update <- reports_to_get %>%
-#   mutate(year = report_year) %>%
-#   select(colnames(tbl(conn, "annual_reports_status"))) %>%
-#   mutate(datetime_fetched = Sys.time(),
-#          fetched_status = map_chr(report_resps, "ingest_status"))
-#
-#
-#
-# removed <- update_sql_table(conn, "annual_reports_status", reports_to_update, c("country", "code", "year", "semester"))
-
-
-
-
