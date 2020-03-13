eaf650b3840855703c51637587db587c831045fa
Noam Ross (the Robot Version) <ross@ecohealthalliance.org>
2020-03-13 14:20:48 -0400

    Make /scraper the default startup dir

--- scraper/start.sh
+++ scraper/start.sh
@@ -5,6 +5,7 @@
 env >> /usr/local/lib/R/etc/Renviron.site
 
 if [ $REPEL_TEST == "1" ]; then
+  echo "session-default-working-dir=~/repel-infrastructure/scraper" >> /etc/rstudio/rsession.conf
   exec /init
 else
   exec supercronic /home/rstudio/repel-infrastructure/scraper/scrape-schedule.cron
