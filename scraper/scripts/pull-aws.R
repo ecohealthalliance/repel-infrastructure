f81bd5444527a9588d603217cb91b26006dc1baf
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-03-25 15:50:31 -0400

    annotations and no longer need use_credentials call

--- scraper/scripts/pull-aws.R
+++ scraper/scripts/pull-aws.R
@@ -1,19 +1,18 @@
+# This script pulls downloaded outbreak and annual reports from AWS
+# These are used to initiate the wahis database without having to redownload all files
+
 library(aws.s3)
 library(fs)
 
-aws.signature::use_credentials()
-Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
-
-setwd("/Users/emmamendelsohn/r_projects/repel-infrastructure/scraper")
-
+# Specify files and directory ---------------------------------------------
 dir <- "data-raw"
 if(dir != "." && !dir_exists(dir)){
   dir_create(here::here(dir))
 }
-
 bucket <- "wahis-data"
 objects <- c("wahis-raw-annual-reports.tar.xz", "wahis-raw-outbreak-reports.tar.xz")
 
+# Download  ---------------------------------------------
 purrr::walk(objects, function(object){
 
   # Download the file
@@ -25,5 +24,4 @@ purrr::walk(objects, function(object){
   # Uncompress the file
   untar(object, tar = "internal", exdir = dir)
   file.remove(object)
-
 })
