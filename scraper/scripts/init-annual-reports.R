23f9f574158304869cb50c5f38fdce503aa37adc
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-04-24 15:24:03 -0400

    renaming, add field check

--- scraper/scripts/init-annual-reports.R
+++ scraper/scripts/init-annual-reports.R
@@ -12,9 +12,9 @@ conn <- wahis_db_connect()
 
 #Remove old tables ----------------------------
 db_tables <- db_list_tables(conn)
-db_tables_wahis <- db_tables[grepl("annual_reports_", db_tables)]
+db_tables_annual <- db_tables[grepl("annual_reports_", db_tables)]
 
-walk(db_tables_wahis, ~dbRemoveTable(conn, .))
+walk(db_tables_annual, ~dbRemoveTable(conn, .))
 
 # List all annual report files to ingest ---------------------------------------------------------
 filenames <- list.files(here::here("data-raw/wahis-raw-annual-reports"),
@@ -38,14 +38,14 @@ annual_reports <-  readr::read_rds(here::here("data-intermediate", "wahis_ingest
 assertthat::are_equal(length(filenames), length(annual_reports))
 ingest_status_log <- tibble(web_page = basename(filenames),
                             ingest_status = map_chr(annual_reports, ~.x$ingest_status)) %>%
-  mutate(code = substr(web_page, 1, 3),
+  mutate(country_iso3c = substr(web_page, 1, 3),
          report_year = substr(web_page, 5, 8),
-         semester = substr(web_page, 13, 13),
-         report = paste(code, report_year, semester, sep ="_")) %>%
+         report_semester = substr(web_page, 13, 13),
+         report = paste(country_iso3c, report_year, report_semester, sep ="_")) %>%
   select(-web_page) %>%
   mutate(in_database = ingest_status == "available") %>%
   mutate(ingest_error = ifelse(!in_database, ingest_status, NA)) %>%
-  select(report, code, report_year, semester, in_database, ingest_error)
+  select(report, country_iso3c, report_year, report_semester, in_database, ingest_error)
 
 annual_reports_transformed <- wahis::transform_annual_reports(annual_reports)
 write_rds(annual_reports_transformed, here::here("data-intermediate", "wahis_transformed_annual_reports.rds"))
@@ -58,6 +58,8 @@ iwalk(annual_reports_transformed,
 )
 
 dbWriteTable(conn,  name = "annual_reports_ingest_status_log", value = ingest_status_log)
+
+field_check(conn, "annual_reports_") #
 
 dbDisconnect(conn)
 
