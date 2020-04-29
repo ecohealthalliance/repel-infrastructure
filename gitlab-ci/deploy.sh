4080069e30283dc67bf1eb8e316b4ffad1cfe8ab
Rob Young <young@ecohealthalliance.org>
2020-04-29 14:46:40 -0400

    last gitlab-ci script added

---
+++ gitlab-ci/deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function restore_pg () {
+  echo ${DEPLOYMENT_SERVER_PASS} | sudo -S ls \
+    && cd ${DEPLOYMENT_SERVER_DIR}; sudo docker-compose down --volumes
+}
+
+function compose_up () {
+  echo ${DEPLOYMENT_SERVER_PASS} | sudo -S ls \
+    && echo ${CI_REGISTRY_PASSWORD} \
+       | sudo docker login -u ${CI_REGISTRY_USER} --password-stdin ${CI_REGISTRY}; cd ${DEPLOYMENT_SERVER_DIR}; sudo docker-compose pull; sudo docker-compose up -d
+}
+
+set -a
+source .env
+set +a
+
+sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
+    ssh -p "${DEPLOYMENT_SERVER_SSH_PORT}" \
+        -o StrictHostKeyChecking=no \
+        -o PreferredAuthentications=password \
+        -o PubkeyAuthentication=no \
+        ${DEPLOYMENT_SERVER_USER}@${DEPLOYMENT_SERVER_URL} \
+        "mkdir -p ~/${DEPLOYMENT_SERVER_DIR}"
+
+sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
+    scp -r -P "${DEPLOYMENT_SERVER_SSH_PORT}" \
+        -o StrictHostKeyChecking=no \
+        -o PreferredAuthentications=password \
+        -o PubkeyAuthentication=no \
+        $(pwd) ${DEPLOYMENT_SERVER_USER}@${DEPLOYMENT_SERVER_URL}:~/
+
+if [[ "$RESTORE_PG_FROM_AWS" == "1" ]]
+then
+  sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
+      ssh -p "${DEPLOYMENT_SERVER_SSH_PORT}" \
+          -o StrictHostKeyChecking=no \
+          -o PreferredAuthentications=password \
+          -o PubkeyAuthentication=no \
+          ${DEPLOYMENT_SERVER_USER}@${DEPLOYMENT_SERVER_URL} \
+          restore_pg
+fi
+
+sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
+    ssh -p "${DEPLOYMENT_SERVER_SSH_PORT}" \
+        -o StrictHostKeyChecking=no \
+        -o PreferredAuthentications=password \
+        -o PubkeyAuthentication=no \
+        ${DEPLOYMENT_SERVER_USER}@${DEPLOYMENT_SERVER_URL} \
+        compose_up
