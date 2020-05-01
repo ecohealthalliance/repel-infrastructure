8e312f42a2faf2d8766315d14de65d9675a95c57
Rob Young <young@ecohealthalliance.org>
2020-05-01 10:19:10 -0400

    removed functions from deploy script

--- gitlab-ci/deploy.sh
+++ gitlab-ci/deploy.sh
@@ -4,15 +4,6 @@ set -a
 source .env
 set +a
 
-restore_pg () {
-  echo ${DEPLOYMENT_SERVER_PASS} | sudo -S ls \
-    && cd ${DEPLOYMENT_SERVER_DIR}; sudo docker-compose down --volumes;
-}
-
-compose_up () {
-  echo ${DEPLOYMENT_SERVER_PASS} | sudo -S ls && echo ${CI_REGISTRY_PASSWORD} | sudo docker login -u ${CI_REGISTRY_USER} --password-stdin ${CI_REGISTRY}; cd ${DEPLOYMENT_SERVER_DIR}; sudo docker-compose pull; sudo docker-compose up -d;
-}
-
 sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
     ssh -p "${DEPLOYMENT_SERVER_SSH_PORT}" \
         -o StrictHostKeyChecking=no \
@@ -36,7 +27,9 @@ then
           -o PreferredAuthentications=password \
           -o PubkeyAuthentication=no \
           ${DEPLOYMENT_SERVER_USER}@${DEPLOYMENT_SERVER_URL} \
-          "$(typeset -f restore_pg); restore_pg"
+          "echo ${DEPLOYMENT_SERVER_PASS} | sudo -S ls \
+            && cd ${DEPLOYMENT_SERVER_DIR}; \
+               sudo docker-compose down --volumes;"
 fi
 
 sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
@@ -45,4 +38,9 @@ sshpass -p "${DEPLOYMENT_SERVER_PASS}" \
         -o PreferredAuthentications=password \
         -o PubkeyAuthentication=no \
         ${DEPLOYMENT_SERVER_USER}@${DEPLOYMENT_SERVER_URL} \
-        "$(typeset -f compose_up); echo compose_up; compose_up"
+        "echo ${DEPLOYMENT_SERVER_PASS} | sudo -S ls \
+          && echo ${CI_REGISTRY_PASSWORD} \
+            | sudo docker login -u ${CI_REGISTRY_USER} --password-stdin ${CI_REGISTRY}; \
+              cd ${DEPLOYMENT_SERVER_DIR}; \
+              sudo docker-compose pull; \
+              sudo docker-compose up -d;"
