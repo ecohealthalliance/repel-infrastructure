7f05fec8ee3b33956a672dd1c0cd6fa779657ea3
RobJY <young@ecohealthalliance.org>
2020-05-08 09:34:34 -0400

    Workflows (#21)
    * added docker-compose-dev to fix cron on dev server
    
    * wrap docker compose up with single quotes in deply script
    
    * removed docker compose flags to test
    
    * added backup_flag=yes to main docker-compose yaml file
    
    * changed .env deployment server dir
    
    * delete CSVs before pushing new ones to aws in cron
    
    * modified aws rm command
    
    * testing first script for gitlab-ci
    
    * fixed permissions
    
    * more converting gitlab-ci to scripts
    
    * last gitlab-ci script added
    
    * bug fix: removed function keyword from deploy script
    
    * put back docker login password-stdin and bug fix
    
    * still trying to get function syntax right
    
    * forcing the shell in deploy.sh
    
    * removed functions from deploy script
    
    * added functions to deploy.sh
    
    * bug fix: added missing backslash
    
    * removed one function call for test
    
    * test remove linebreaks in fucntion
    
    * echo function contents for debugging
    
    * removed functions from deploy script
    
    * moved ports into base compose yml file
    
    * added variables to .env and repeluser to postgres Dockerfile
    
    * removed shell for new repeluser in postgres container
    
    * we don't need user in container after all
    
    * push and pull scripts ready and testing new deply.sh
    
    * added functions for push to dev and AWS  and added CSV to push to AWS

--- scraper/push_db_aws.sh
+++ scraper/push_db_aws.sh
@@ -5,3 +5,9 @@ export PGHOST=$POSTGRES_HOST
 export PGDATABASE=$POSTGRES_DB
 
 pg_dumpall | xz -9 -c | aws s3 cp - s3://${AWS_BUCKET}/dumps/${PGDUMP_FILENAME}.xz
+
+psql -Atc "select tablename from pg_tables where schemaname='public'" |\
+  while read TBL; do
+    sem -j 10 "psql -c \"COPY $TBL TO STDOUT WITH NULL AS 'NA' CSV HEADER\" | xz -9 -c | aws s3 cp - s3://${AWS_BUCKET}/csv/${TBL}.csv.xz"
+  done
+sem --wait
---
+++ scraper/push_db_local_to_aws_and_dev.sh
@@ -0,0 +1,67 @@
+# copy local repel database to dev and AWS
+
+# functions to test environment variables to local or dev server
+
+set_local_env () {
+  export PGUSER=$POSTGRES_USER
+  export PGPASSWORD=$POSTGRES_PASSWORD
+  export PGPORT=$POSTGRES_PORT
+  export PGHOST=$POSTGRES_HOST
+  export PGDATABASE=$POSTGRES_DB
+}
+
+set_dev_env () {
+  export PGUSER=$DEPLOYMENT_SERVER_USER
+  export PGPASSWORD=$DEPLOYMENT_SERVER_DB_PASS
+  export PGPORT=$DEPLOYMENT_SERVER_PSQL_PORT
+  export PGHOST=$DEPLOYMENT_SERVER_URL
+  export PGDATABASE=$POSTGRES_DB
+}
+
+# verify with user that this should be run
+echo "Running this script will push your local repel database to the development server and to AWS."
+read -p "Are you sure you want to proceed? (Y/N) " -r
+if [[ ! $REPLY == "Y" ]]
+then
+  echo "Exiting without running as requested."
+  exit 0
+fi
+
+# set env to local and dump database
+
+set_local_env
+pg_dump repel > /tmp/repel_backup_local.dmp
+
+# set env to dev server and update database
+
+set_dev_env
+dropdb repel || { echo "Error: failed to drop repel database!" && exit 1; }
+createdb repel || { echo "Error: failed to create repel database!" && exit 1; }
+psql repel < /tmp/repel_backup_local.dmp || { echo "Error: failed to restore repel database from backup!" && exit 1; }
+
+# archive dump and push to AWS
+
+xz /tmp/repel_backup_local.dmp
+aws s3 cp /tmp/repel_backup_local.dmp.xz s3://${AWS_BUCKET}/dumps/${PGDUMP_FILENAME}.xz
+
+# remove existing csv files from AWS then copy local ones
+
+aws s3 rm s3://${AWS_BUCKET}/csv --recursive
+
+# The `sem` command allows up to `j` jobs to be run in the background,
+# Then `sem --wait` pauses execution until they are all done.  Doing this to
+# allow parallel, but limited, connections to the database.
+
+# set env back to local to copy CSVs to AWS
+
+set_local_env
+
+psql -Atc "select tablename from pg_tables where schemaname='public'" |\
+  while read TBL; do
+    sem -j 10 "psql -c \"COPY $TBL TO STDOUT WITH NULL AS 'NA' CSV HEADER\" | xz -9 -c | aws s3 cp - s3://${AWS_BUCKET}/csv/${TBL}.csv.xz"
+  done
+sem --wait
+
+# clean up
+
+rm /tmp/repel_backup_local.dmp*
