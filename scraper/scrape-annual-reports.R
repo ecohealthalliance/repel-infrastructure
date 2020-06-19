a21826d263559707d7bbb274e77e6bfd6113d705
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2020-06-19 09:58:41 -0400

    save QA markdowns to reports dir

--- scraper/scrape-annual-reports.R
+++ scraper/scrape-annual-reports.R
@@ -92,7 +92,7 @@ assert_that(dbExistsTable(conn, "annual_reports_animal_diseases"))
 safely(rmarkdown::render, quiet = FALSE)(
   "qa-annual-reports.Rmd",
   output_file = paste0("annual-report-qa.html"),
-  output_dir = "qa-reports"
+  output_dir = "reports"
 )
 
 dbDisconnect(conn)
