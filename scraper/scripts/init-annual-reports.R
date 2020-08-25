f52495f0926476482c976db1953be4ee6e9bf291
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2020-08-25 13:00:14 -0400

    updating init routine to work outside scraper container

--- scraper/scripts/init-annual-reports.R
+++ scraper/scripts/init-annual-reports.R
@@ -1,11 +1,12 @@
 # This is to manually add downloaded annual reports to database
 # First run scripts/pull-aws-downloaded-wahis to download the files
 
-source(here::here("packages.R"))
-source(here::here("functions.R"))
+source(here::here("scraper/packages.R"))
+source(here::here("scraper/functions.R"))
 library(fs)
 library(future)
 library(furrr)
+library(tictoc)
 
 # Connect to database ----------------------------
 conn <- wahis_db_connect()
@@ -17,23 +18,22 @@ db_tables_annual <- db_tables[grepl("annual_reports_", db_tables)]
 walk(db_tables_annual, ~dbRemoveTable(conn, .))
 
 # List all annual report files to ingest ---------------------------------------------------------
-filenames <- list.files(here::here("data-raw/wahis-raw-annual-reports"),
+filenames <- list.files(here::here("scraper/data-raw/wahis-raw-annual-reports"),
                         pattern = "*.html",
                         full.names = TRUE)
 
 # Set up parallel plan  --------------------------------------------------------
-plan(multiprocess) # This takes a bit to load on many cores as all the processes are starting
+plan("multisession") # This takes a bit to load on many cores as all the processes are starting
 
 # Run ingest (~35 mins) ---------------------------------------------------------
 message(paste(length(filenames), "files to process"))
-library(tictoc)
 tic()
 wahis_annual <- future_map(filenames, safe_ingest_annual, .progress = TRUE)
 toc()
-write_rds(wahis_annual, here::here("data-intermediate", "wahis_ingested_annual_reports.rds"))
+write_rds(wahis_annual, here::here("scraper", "data-intermediate", "wahis_ingested_annual_reports.rds"))
 
 # Transform files   ------------------------------------------------------
-annual_reports <-  readr::read_rds(here::here("data-intermediate", "wahis_ingested_annual_reports.rds"))
+annual_reports <-  readr::read_rds(here::here("scraper", "data-intermediate", "wahis_ingested_annual_reports.rds"))
 
 assertthat::are_equal(length(filenames), length(annual_reports))
 ingest_status_log <- tibble(web_page = basename(filenames),
@@ -47,11 +47,13 @@ ingest_status_log <- tibble(web_page = basename(filenames),
   mutate(ingest_error = ifelse(!in_database, ingest_status, NA)) %>%
   select(report, country_iso3c, report_year, report_semester, in_database, ingest_error)
 
-annual_reports_transformed <- wahis::transform_annual_reports(annual_reports)
-write_rds(annual_reports_transformed, here::here("data-intermediate", "wahis_transformed_annual_reports.rds"))
+tic()
+annual_reports_transformed <- transform_annual_reports(annual_reports)
+toc()
+write_rds(annual_reports_transformed, here::here("scraper", "data-intermediate", "wahis_transformed_annual_reports.rds"))
 
 # Save files to db --------------------------------------------------------
-annual_reports_transformed <- read_rds(here::here("data-intermediate", "wahis_transformed_annual_reports.rds"))
+annual_reports_transformed <- read_rds(here::here("scraper", "data-intermediate", "wahis_transformed_annual_reports.rds"))
 
 iwalk(annual_reports_transformed,
       ~dbWriteTable(conn,  name = .y, value = .x)
