fa3aa2b62ce96670f9f1beeff8be9c090c1f1e3c
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2020-09-22 11:53:09 -0400

    dynamic database host/port depending on working environment

--- scraper/functions.R
+++ scraper/functions.R
@@ -2,12 +2,25 @@
 
 # Connect to WAHIS Database
 wahis_db_connect <- function(){
-  env_file <- str_remove(here::here(".env"), "scraper/")
-  base::readRenviron(env_file)
+  # read env file
+  env_file <- stringr::str_remove(here::here(".env"), "scraper/")
+  base:: readRenviron(env_file)
+
+  # set host and port depending on if running dev or production
+  dev_host <- stringr::str_extract( Sys.info()["nodename"], "aegypti|prospero")
+  if(is.na(dev_host)){
+    host <- Sys.getenv("POSTGRES_HOST")
+    port <- Sys.getenv("POSTGRES_PORT")
+  }else{
+    host <- paste0(dev_host, ".ecohealthalliance.org")
+    port <- "22053"
+  }
+
+  # connect
   conn <- dbConnect(
     RPostgres::Postgres(),
-    host = "aegypti.ecohealthalliance.org",
-    port = "22053",
+    host = host,
+    port = port,
     user = Sys.getenv("POSTGRES_USER"),
     password = Sys.getenv("POSTGRES_PASSWORD"),
     dbname = Sys.getenv("POSTGRES_DB")
