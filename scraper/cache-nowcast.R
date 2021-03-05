4b60cf3a4be6a467c7cea8c6a7ee1283cb0ef21a
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-03-05 18:53:24 +0000

    more robust cache nowcast (in progress)

--- scraper/cache-nowcast.R
+++ scraper/cache-nowcast.R
@@ -25,18 +25,54 @@ if(dbExistsTable(conn,  "nowcast_boost_augment_predict")){
 
 if(!dbExistsTable(conn,  "nowcast_boost_augment_predict")   | db_disease_status_etag != aws_disease_status_etag | db_cases_etag != aws_cases_etag){
 
-  # Cache full database predictions ---------------------------------------------------
-  repeldat <- repelpredict:::split_annual_reports_animal_hosts(conn) %>%
-    select(all_of(repelpredict:::grouping_vars), validation_set) %>%
-    distinct()
+  # Cache database predictions ---------------------------------------------------
 
   model_object <-  nowcast_boost_model(
     disease_status_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "boost_mod_disease_status.rds"),
     cases_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "boost_mod_cases.rds"))
 
+  # get full database
+  repeldat <- repelpredict::repel_split(model_object, conn)
+
+  # expand taxa+disease combos to all countries and years (including those not reported)
+  reported_years <- repeldat %>%
+    arrange(report_year, report_semester) %>%
+    distinct(report_year, report_semester)
+
+  last_sem <- reported_years %>%
+    tail(1)
+
+  if(last_sem$report_semester == 2){
+    next_year <- tibble(report_year = rep(max(last_sem$report_year)+1, each = 2), report_semester = 1:2)
+  }else{
+    next_year <- tibble(report_year = c(max(last_sem$report_year), max(last_sem$report_year) + 1), report_semester = 2:1)
+  }
+
+  years_to_expand <- bind_rows(reported_years, next_year)
+
+  repeldat_expand <- repeldat %>%
+    distinct(disease, disease_population, taxa) %>%
+    expand_grid(country_iso3c = unique(repeldat$country_iso3c),
+                years_to_expand) %>%
+    arrange(country_iso3c, taxa, disease, disease_population, report_year, report_semester)
+
+  # note which diseases were reported
+  repeldat_reported <- repeldat %>%
+    select(colnames(repeldat_expand)) %>%
+    mutate(reported = TRUE)
+
+  # run prediction (slow)
+
+  augmented_data <- repel_augment(model_object, conn, repeldat_expand)
+  write_rds(augmented_data, "tmp_augmented_data.rds")
+
+  # TODO need to rerun model - xgboost and tidymodel compatability issue
+  predictions <- repel_predict(model_object, newdata = augmented_data)
+
+
   forecasted_repeldat <- repel_forecast(model_object = model_object,
                                         conn = conn,
-                                        newdata = repeldat,
+                                        newdata = repeldat_expand,
                                         use_cache = FALSE)
 
   forecasted_repeldat <- forecasted_repeldat[[1]] %>%
