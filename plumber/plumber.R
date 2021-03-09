046b53234acf2a143e664124b93f65c174d1b484
Rob Young <young@ecohealthalliance.org>
2021-03-09 10:32:16 -0500

    added memoization

--- plumber/plumber.R
+++ plumber/plumber.R
@@ -1,4 +1,17 @@
 library(DBI)
+library(RPostgreSQL)
+library(stringr)
+library(jsonlite)
+library(memoise)
+
+conn <- dbConnect(
+  RPostgres::Postgres(),
+  host = Sys.getenv("POSTGRES_HOST"),
+  port = Sys.getenv("POSTGRES_PORT"),
+  user = Sys.getenv("POSTGRES_USER"),
+  password = Sys.getenv("POSTGRES_PASSWORD"),
+  dbname = Sys.getenv("POSTGRES_DB")
+)
 
 #* @get /mean
 normalMean <- function(samples=10){
@@ -6,76 +19,93 @@ normalMean <- function(samples=10){
   mean(data)
 }
 
-
-# Host is just "postgres" within the docker network, with port 5432
-# Connect to WAHIS Database
-wahis_db_connect <- function(){
-  # read env file
-  #env_file <- stringr::str_remove(here::here(".env"), "plumber/")
-  env_file <- ".env"
-  base:: readRenviron(env_file)
-
-  # set host and port depending on if running dev or production
-  dev_host <- stringr::str_extract( Sys.info()["nodename"], "aegypti|prospero")
-  if(is.na(dev_host)){
-    host <- Sys.getenv("POSTGRES_HOST")
-    port <- Sys.getenv("POSTGRES_PORT")
-  }else{
-    host <- paste0(dev_host, ".ecohealthalliance.org")
-    port <- "22053"
-  }
-
-  # connect
-  conn <- dbConnect(
-    RPostgres::Postgres(),
-    host = host,
-    port = port,
-    user = Sys.getenv("POSTGRES_USER"),
-    password = Sys.getenv("POSTGRES_PASSWORD"),
-    dbname = Sys.getenv("POSTGRES_DB")
-    )
-
-  return(conn)
+#* @get /listTables
+tableList <- function() {
+  dbListTables(conn)
 }
 
-conn <- wahis_db_connect()
-my_cache <- cachem::cache_disk()
 # TODO: Establish connection in top-level environment so it starts with API
 # If any stability issues (maybe TODO at future date), consider connection pool:
 #   https://db.rstudio.com/pool/
-#* @get /dbtest
-dbvals <- function(){
-  conn <- wahis_db_connect()
-}
-
-#* @post /sum
-addTwo <- function(a, b){
-  as.numeric(a) + as.numeric(b)
-}
 
 # Generic database fetch function
 serialize_repel_data <- function(data, format = c("csv", "json", "rds"), cache = TRUE) {
-  # TODO: use serializers to return the right data format
-
+  if (format == "json") {
+    # data = serializeJSON(data)
+    data = serializer_json(data, type="application/json")
+  } else if (format == "csv") {
+    data
+  } else if (format == "rds") {
+    data
+  }
 }
 
+get_db_results <- function(conn, query) {
+  nowcast_query <- dbSendQuery(conn, query)
+  result <-dbFetch(nowcast_query)
+}
+cache_dir <- cachem::cache_disk("/plumber_cache")
+get_db_results_memo <- memoize(get_db_results, cache = cache_dir)
+
+#* @get /clearcache
+clear_cache <- function() {
+  cache_dir$reset()
+}
 
 #* @get /nowcast_predictions
-get_nowcast_predictions <- memoise_function(function(columns = NULL, years, countries, format, cache = TRUE) {
-  nowcast_predictions_table <- tbl(conn, "nowcast_boost_augment_predict")
-  if (!is.null(columns)) {
-    nowcast_predictions_table <- select(nowcast_predictions_table, columns)
+get_nowcast_predictions <- function(columns = NULL, years, countries, format=c("csv","json","rds"), cache = TRUE) {
+
+  #nowcast_predictions_table <- tbl(conn, "nowcast_boost_augment_predict")
+  #dat <- collect(nowcast_predictions_table)
+
+  columns
+  if (is.null(columns)) {
+    columns = "*"
   }
-  nowcast_predictions_table <- filter(nowcast_predictions_table, report_year %in% years)
-  dat <- collect(nowcast_predictions_table)
-  serialize_repel_data(dat, format, cache)
-})
+  if (years == "all") {
+    years = ""
+  }
+  if (countries == "all"){
+    countries = ""
+  } else {
+    countries_list = strsplit(countries, split=",")[[1]]
+    countries = paste("'",countries_list,"'", sep='', collapse=",")
+  }
 
-memoise_function <- function(fn) {
-  #use cachem::cache_disk (like connection, set up at top so it exists on startup)
-  # disk-mount the cache directory so it survives restart
-  memoise::memoise(fn, cache = my_cache)
+  if (years == "" & countries == ""){
+    where_clause = ""
+  } else if (years == "") {
+    where_clause = paste("WHERE country_iso3c IN (", countries, ")")
+  } else if (countries == "") {
+    where_clause = paste("WHERE report_year IN (", years, ")")
+  } else {
+    where_clause = paste("WHERE country_iso3c IN (", countries, ") AND report_year in (", years, ")")
+  }
 
+  query = paste("SELECT", columns, "FROM nowcast_boost_augment_predict", where_clause, "LIMIT 1000", sep=" ")
 
+  #nowcast_query <- dbSendQuery(conn, query)
+  #result <-dbFetch(nowcast_query)
+
+  if (cache) {
+    result <- get_db_results_memo(conn, query)
+  } else {
+    result <- get_db_results(conn, query)
+  }
+  # serialize_repel_data(result, format, cache)
 }
+#get_nowcast_predictions <- memoise_function(function(columns = NULL, years, countries, format, cache = TRUE) {
+#  nowcast_predictions_table <- tbl(conn, "nowcast_boost_augment_predict")
+#  if (!is.null(columns)) {
+#    nowcast_predictions_table <- select(nowcast_predictions_table, columns)
+#  }
+#  nowcast_predictions_table <- filter(nowcast_predictions_table, report_year %in% years)
+#  dat <- collect(nowcast_predictions_table)
+#  serialize_repel_data(dat, format, cache)
+#})
 
+#memoise_function <- function(fn) {
+#  #use cachem::cache_disk (like connection, set up at top so it exists on startup)
+#  # disk-mount the cache directory so it survives restart
+#  memoise::memoise(fn, cache = my_cache)
+#}
