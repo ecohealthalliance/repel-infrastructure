a7413ce171349f36cba810ffa310db9ff15691f5
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-03-09 19:22:30 +0000

    leaflet render fixes

--- shiny/content/nowcast/app.R
+++ shiny/content/nowcast/app.R
@@ -17,7 +17,10 @@ admin <- ne_countries(type='countries', scale = 'medium', returnclass = "sf") %>
     filter(name != "Antarctica") %>%
     select(country_iso3c = iso_a3, geometry)
 
-nowcast_predicted_sum <- read_csv(here::here("shiny", "content", "nowcast", "data", "nowcast_predicted_sum.csv"))
+nowcast_predicted_sum <- read_csv(here::here("shiny", "content", "nowcast", "data", "nowcast_predicted_sum.csv")) %>%
+    mutate(status_coalesced = factor(status_coalesced, levels = c("reported present", "unreported, predicted present",  "reported absent", "unreported, predicted absent")))
+
+pal <- colorFactor(palette = c("#E31A1C", "#FB9A99", "#1F78B4", "#A6CEE3"), domain = levels(nowcast_predicted_sum$status_coalesced))
 
 oie_diseases <- repelpredict:::get_oie_high_importance_diseases()
 
@@ -25,45 +28,52 @@ oie_diseases <- repelpredict:::get_oie_high_importance_diseases()
 
 ui <- fluidPage(
     navbarPage("REPEL Nowcast", id="main",
-                 tabPanel("Map",
-                          # dropdown
-                          selectInput(inputId = "select_disease", label = "Select OIE disease", choices = oie_diseases),
-                          sliderInput(inputId = "select_year", label = "Select year", min = min(nowcast_predicted_sum$report_year), max = max(nowcast_predicted_sum$report_year), value = 2019, sep = ""),
-                          radioButtons(inputId = "select_semester", label = "Select semester", choices = c("Jan - June", "July - Dec"), inline = TRUE),
-                          leafletOutput(outputId = "diseasemap", height=1000)),
-                 tabPanel("Time series", plotOutput("data")))
+               tabPanel("Map",
+                        # dropdown
+                        selectInput(inputId = "select_disease", label = "Select OIE disease", choices = oie_diseases),
+                        sliderInput(inputId = "select_year", label = "Select year", min = min(nowcast_predicted_sum$report_year), max = max(nowcast_predicted_sum$report_year), value = 2019, sep = ""),
+                        radioButtons(inputId = "select_semester", label = "Select semester", choices = c("Jan - June", "July - Dec"), inline = TRUE),
+                        leafletOutput(outputId = "diseasemap", height=1000)),
+               tabPanel("Time series", plotOutput("data")))
 )
 
 server <- function(input, output) {
 
     output$diseasemap <- renderLeaflet({
+
+        select_semester <- switch(input$select_semester, "Jan - June" = 1, "July - Dec" = 2)
+
         mapdat <- nowcast_predicted_sum %>%
-            filter(disease == input$select_disease, report_year == input$select_year, report_semester == input$select_semester) %>%
-            left_join(admin)
-        pal <- colorFactor(palette = c("#E31A1C", "#FB9A99", "#1F78B4", "#A6CEE3"), domain = levels(mapdat$status_coalesced))
+            filter(disease == input$select_disease, report_year == input$select_year, report_semester == select_semester) #%>%
+           # filter(disease == oie_diseases[[1]], report_year == 2019, report_semester == 1)
+
+        admin_mapdat <- admin %>%
+            right_join(mapdat) %>%
+            left_join(tibble(fill_color = c("#E31A1C", "#FB9A99", "#1F78B4", "#A6CEE3"), status_coalesced =  levels(nowcast_predicted_sum$status_coalesced)))
 
         leaflet() %>%
             addProviderTiles("CartoDB.DarkMatter") %>%
-            addPolygons(data = mapdat, weight = 0.5, smoothFactor = 0.5,
-                        opacity = 0.5,  color = ~fill,
-                        fillOpacity = 0.75, fillColor = ~fill,
+            # setView(lng = 144, lat = -37, zoom = 1) %>%
+            addPolygons(data = admin_mapdat, weight = 0.5, smoothFactor = 0.5,
+                        opacity = 0.5,  color = ~fill_color,
+                        fillOpacity = 0.75, fillColor = ~fill_color,
                         label = ~tooltip_lab) %>%
-            addLegend(pal = pal, values = levels(mapdat$status_coalesced), position = "bottomright",
-                      labFormat = labelFormat(transform = function(x) levels(mapdat$status_coalesced)))
+            addLegend(pal = pal, values = levels(nowcast_predicted_sum$status_coalesced), position = "bottomright",
+                      labFormat = labelFormat(transform = function(x) levels(nowcast_predicted_sum$status_coalesced)))
     })
 
 
 
 }
-    # User options: Select Disease
-    # Map: showing currently reported, not currently reported/predicted
-    #  - 4 color, or maybe color + hatched in future iteration
-    #  - Slider to change time point?
-    # Map: Predicted in six months
-    #  Use leaflet with no base to start
-    # Shiny: Click on a country, get a time series
-    # Time series plot(s):
-    #  - Show presence, cases reported and predicted (emphasize gaps!)
-    #  - Hover to show values
-    #  -
+# User options: Select Disease
+# Map: showing currently reported, not currently reported/predicted
+#  - 4 color, or maybe color + hatched in future iteration
+#  - Slider to change time point?
+# Map: Predicted in six months
+#  Use leaflet with no base to start
+# Shiny: Click on a country, get a time series
+# Time series plot(s):
+#  - Show presence, cases reported and predicted (emphasize gaps!)
+#  - Hover to show values
+#  -
 shinyApp(ui = ui, server = server)
