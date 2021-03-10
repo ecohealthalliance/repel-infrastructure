40290e4195c8d9c056f1d538c8211d159f135d2e
Rob Young <young@ecohealthalliance.org>
2021-03-10 10:37:26 -0500

    added serialization

--- plumber/plumber.R
+++ plumber/plumber.R
@@ -1,8 +1,11 @@
+library(plumber)
 library(DBI)
 library(RPostgreSQL)
 library(stringr)
 library(jsonlite)
 library(memoise)
+
+## util functions ################################
 
 conn <- dbConnect(
   RPostgres::Postgres(),
@@ -13,59 +16,56 @@ conn <- dbConnect(
   dbname = Sys.getenv("POSTGRES_DB")
 )
 
-#* @get /mean
-normalMean <- function(samples=10){
-  data <- rnorm(samples)
-  mean(data)
-}
-
-#* @get /listTables
-tableList <- function() {
-  dbListTables(conn)
-}
-
-# TODO: Establish connection in top-level environment so it starts with API
-# If any stability issues (maybe TODO at future date), consider connection pool:
-#   https://db.rstudio.com/pool/
-
-# Generic database fetch function
-serialize_repel_data <- function(data, format = c("csv", "json", "rds"), cache = TRUE) {
+serialize_repel_data <- function(data, format = c("csv", "json", "rds")) {
   if (format == "json") {
-    # data = serializeJSON(data)
-    data = serializer_json(data, type="application/json")
+    jsonlite::toJSON(data)
   } else if (format == "csv") {
-    data
+    readr::format_csv(data)
   } else if (format == "rds") {
-    data
+    base::serialize(data, connection = NULL)
   }
 }
 
+cache_dir <- cachem::cache_disk("/plumber_cache")
+
+serialize_repel_data_memo <- memoize(serialize_repel_data, cache = cache_dir)
+
 get_db_results <- function(conn, query) {
   nowcast_query <- dbSendQuery(conn, query)
-  result <-dbFetch(nowcast_query)
+  result <-dbFetch(nowcast_query, n = -1)
 }
-cache_dir <- cachem::cache_disk("/plumber_cache")
 get_db_results_memo <- memoize(get_db_results, cache = cache_dir)
 
+
+## API endpoints #######################################
+
+### /clearcache  - clears any existing cache files
 #* @get /clearcache
 clear_cache <- function() {
   cache_dir$reset()
 }
 
+### /nowcast_predictions  - returns rows from db query of nowcast_boost_augment_predict table
+### parameters:
+###     columns: comma separated list of columns desired or NULL for all (default = NULL)
+###     years: comma separated list of years desired or NULL for all (default = NULL)
+###     countries: comma separated list of countries desired or NULL for all (default = NULL)
+###     format: output format.  Allowed options are csv, json or rds.
+###     cache: use cached results if available.  Allowed options are TRUE or FALSE (default = TRUE)
+###     limit: limit the number of rows output.  Allowed options are an integer or NULL for all (default = NULL)
+#' @serializer contentType list(type="application/octet-stream")
 #* @get /nowcast_predictions
-get_nowcast_predictions <- function(columns = NULL, years, countries, format=c("csv","json","rds"), cache = TRUE) {
+get_nowcast_predictions <- function(columns = NULL, years = NULL, countries = NULL, format=c("csv","json","rds"), cache = TRUE, limit = NULL) {
 
-  #nowcast_predictions_table <- tbl(conn, "nowcast_boost_augment_predict")
-  #dat <- collect(nowcast_predictions_table)
-
-  columns
   if (is.null(columns)) {
     columns = "*"
   }
-  if (years == "all") {
+
+  if (is.null(years)) {
     years = ""
   }
-  if (countries == "all"){
+
+  if (is.null(countries)) {
     countries = ""
   } else {
     countries_list = strsplit(countries, split=",")[[1]]
@@ -82,30 +82,22 @@ get_nowcast_predictions <- function(columns = NULL, years, countries, format=c("
     where_clause = paste("WHERE country_iso3c IN (", countries, ") AND report_year in (", years, ")")
   }
 
-  query = paste("SELECT", columns, "FROM nowcast_boost_augment_predict", where_clause, "LIMIT 1000", sep=" ")
+  limit_str = ""
+  if (!is.null(limit)) {
+    limit_str = paste("LIMIT ", limit)
+  }
 
-  #nowcast_query <- dbSendQuery(conn, query)
-  #result <-dbFetch(nowcast_query)
+  query = paste("SELECT", columns, "FROM nowcast_boost_augment_predict", where_clause, limit_str, sep=" ")
 
   if (cache) {
     result <- get_db_results_memo(conn, query)
   } else {
     result <- get_db_results(conn, query)
   }
-  # serialize_repel_data(result, format, cache)
-}
-#get_nowcast_predictions <- memoise_function(function(columns = NULL, years, countries, format, cache = TRUE) {
-#  nowcast_predictions_table <- tbl(conn, "nowcast_boost_augment_predict")
-#  if (!is.null(columns)) {
-#    nowcast_predictions_table <- select(nowcast_predictions_table, columns)
-#  }
-#  nowcast_predictions_table <- filter(nowcast_predictions_table, report_year %in% years)
-#  dat <- collect(nowcast_predictions_table)
-#  serialize_repel_data(dat, format, cache)
-#})
 
-#memoise_function <- function(fn) {
-#  #use cachem::cache_disk (like connection, set up at top so it exists on startup)
-#  # disk-mount the cache directory so it survives restart
-#  memoise::memoise(fn, cache = my_cache)
-#}
+  if (cache) {
+    serialize_repel_data_memo(result, format)
+  } else {
+    serialize_repel_data(result, format)
+  }
+}
