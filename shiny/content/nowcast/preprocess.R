d05e32dae336dc39cab0a470aea33d6174bcc501
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-03-22 20:57:52 +0000

    remove preprocessing of data. updates to preprocessing of interactive graphs.

--- shiny/content/nowcast/preprocess.R
+++ shiny/content/nowcast/preprocess.R
@@ -1,62 +1,24 @@
 library(tidyverse)
 library(furrr)
-library(rnaturalearth)
-library(sf)
 library(RColorBrewer)
-library(glue)
+library(ggiraph)
+library(stringi)
+library(patchwork)
+library(DBI)
 
-any2 <- function(x) ifelse(all(is.na(x)), NA, any(x, na.rm = TRUE))
-sum2 <- function(x) ifelse(all(is.na(x)), NA, sum(x, na.rm = TRUE))
 
-oie_diseases <- repelpredict:::get_oie_high_importance_diseases()
-
-# conn <- repeldata::repel_remote_conn(
-#     host = "postgres",
-#     port = 5432,
-#     user = "repel_reader",
-#     password = Sys.getenv("REPEL_READER_PASS")
-# )
-
+# repeldata::repel_local_download()
 conn <- repeldata::repel_remote_conn()
 
-nowcast_predicted <- tbl(conn, "nowcast_boost_augment_predict")  %>%
-  select(report_year, report_semester, disease, country_iso3c, taxa,
-         unreported, actual_cases = cases, actual_status = disease_status,  predicted_cases) %>%
-  filter(disease %in% oie_diseases) %>%
-  collect() %>%
-  mutate(predicted_status = predicted_cases > 0,
-         actual_status = as.logical(actual_status),
-         unreported = as.logical(unreported))
-
-write_csv(nowcast_predicted, here::here("shiny", "content", "nowcast", "data", "nowcast_predicted_raw.csv"))
-
-#TODO put into db
-nowcast_predicted_sum <- nowcast_predicted %>%
-  group_by(country_iso3c, disease, report_year, report_semester) %>%
-  summarize(predicted_status = any(predicted_status), actual_status = any2(actual_status),
-            predicted_cases = sum(predicted_cases), actual_cases = sum2(actual_cases), unreported = all(unreported)) %>%
-  ungroup()  %>%
-  mutate(status_coalesced = case_when(
-    actual_status == TRUE ~ "reported present",
-    actual_status == FALSE ~ "reported absent",
-    unreported == TRUE & predicted_status == TRUE ~ "unreported, predicted present",
-    unreported == TRUE & predicted_status == FALSE ~ "unreported, predicted absent",
-  )) %>%
-  mutate(status_coalesced = factor(status_coalesced, levels = c("reported present", "unreported, predicted present",  "reported absent", "unreported, predicted absent"))) %>%
-  mutate(cases_coalesced = coalesce(actual_cases, predicted_cases)) %>%
-  mutate(tooltip_lab = paste0(countrycode::countrycode(country_iso3c, origin = "iso3c", destination = "country.name"),
-                              ":<br>",
-                              if_else(unreported, paste0(" ", cases_coalesced, " predicted cases"), paste0(" ", cases_coalesced, " reported cases"))
-  ))
-
-write_csv(nowcast_predicted_sum, here::here("shiny", "content", "nowcast", "data", "nowcast_predicted_sum.csv"))
-
-# pre generate plots
+nowcast_predicted_sum <- dbReadTable(conn, name = "nowcast_boost_predict_oie_diseases") %>%
+  mutate(status_coalesced = factor(status_coalesced, levels = c("reported present", "unreported, predicted present",  "reported absent", "unreported, predicted absent")))
 
 pred_list <- nowcast_predicted_sum %>%
   mutate(yr = report_year + (report_semester - 1)/2) %>%
-  select(disease, country_iso3c, yr, cases_coalesced, status_coalesced) %>%
-  mutate(label = cases_coalesced) %>% #glue("Reported: ", if_else(missing[type == "Reported"] == "Missing", "Unknown", as.character(cases[type == "Reported"])), "<br/>Predicted: ", cases[type == "Predicted"])) %>%
+  mutate(country_name = countrycode::countrycode(country_iso3c, origin = "iso3c", destination = "country.name")) %>%
+  mutate(disease_clean = stri_trans_totitle(stri_replace_all_fixed(disease, "_", " "))) %>%
+  mutate(cases_coalesced = coalesce(actual_cases, predicted_cases)) %>%
+  mutate(label = paste0("Reported cases = ", replace_na(actual_cases, "missing"), "<br/>Predicted cases = ", predicted_cases)) %>%
   arrange(disease, country_iso3c) %>%
   group_split(disease, country_iso3c)
 
@@ -69,22 +31,34 @@ breaks_by <- function(k) {
 
 plots <- map(pred_list, function(z){
 
- cases_plot <- ggplot(z, aes(x = yr, y = cases_coalesced, color = status_coalesced)) +
-    geom_line() +
-    geom_point_interactive(mapping = aes(tooltip = label, fill = status_coalesced), pch = 21) +
-    labs(title = paste(unique(z$disease), unique(z$country_iso3c), sep = "_")) + # tmp
+  cases_plot <- ggplot(z, aes(x = yr, y = cases_coalesced, fill = status_coalesced, color = status_coalesced)) +
+  #  geom_line() +
+    geom_point_interactive(mapping = aes(tooltip = label, fill = status_coalesced, color = status_coalesced), pch = 21, size = 4) +
+    scale_color_manual(values = c("reported present" = "#E31A1C", "unreported, predicted present" = "#FB9A99",
+                                  "reported absent" = "#1F78B4", "unreported, predicted absent" = "#A6CEE3")) +
+    scale_fill_manual(values = c("reported present" = "#E31A1C", "unreported, predicted present" = "#FB9A99",
+                                  "reported absent" = "#1F78B4", "unreported, predicted absent" = "#A6CEE3")) +
+    labs(title = paste(unique(z$country_name), unique(z$disease_clean), sep = ": "),
+         y = "Cases", fill = "", color = "") +
     theme_minimal() +
     theme(axis.text.x = element_blank(), axis.title.x = element_blank())
 
-  # presence_plot <- ggplot(z, aes(x = yr, y = type, fill = missing)) +
-  #   geom_tile(color = "white") +
-  #   scale_x_continuous(breaks = breaks_by(1)) +
-  #   scale_fill_manual(values = c(Present = "#E31A1C", Absent = "#1F78B4", Missing = "#FFFFFF")) +
-  #   theme_minimal() +
-  #   theme(axis.title.x = element_blank(), panel.grid = element_blank(), axis.title.y = element_blank())
+  zp <- z %>%
+    select(yr, Predicted = predicted_status, Reported = actual_status) %>%
+    pivot_longer(cols = c("Predicted", "Reported"), names_to = "type") %>%
+    mutate(missing = factor(if_else(value, "Present", "Absent", "Missing"), levels = c("Missing", "Present", "Absent")))
 
- girafe(ggobj = cases_plot, #+ presence_plot + plot_layout(ncol = 1, heights = c(0.9, 0.1)),
-        width_svg = 10, height_svg = 4)
+
+  presence_plot <- ggplot(zp, aes(x = yr, y = type, fill = missing)) +
+    geom_tile(color = "white") +
+    scale_x_continuous(breaks = breaks_by(1)) +
+    scale_fill_manual(values = c(Present = "#E31A1C", Absent = "#1F78B4", Missing = "#FFFFFF")) +
+    labs(fill = "", color = "") +
+    theme_minimal() +
+    theme(axis.title.x = element_blank(), panel.grid = element_blank(), axis.title.y = element_blank())
+
+  girafe(ggobj = cases_plot + presence_plot + plot_layout(ncol = 1, heights = c(0.9, 0.1)),
+         width_svg = 10, height_svg = 4)
 })
 
 
@@ -98,19 +72,4 @@ names(plots) <- labs
 
 write_rds(plots,  here::here("shiny", "content", "nowcast", "data", "plots.rds"))
 
-
-
-
-# girafe(ggobj = cases_plot + presence_plot + plot_layout(ncol = 1, heights = c(0.9, 0.1)),
-#        width_svg = 10, height_svg = 4)
-
-
-
-
-
-
-
-
 repeldata::repel_remote_disconnect()
-
- -
