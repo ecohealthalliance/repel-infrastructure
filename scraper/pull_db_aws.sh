ffffb5d974110414b402d9835d5e0081e3a505c4
Noam Ross <ross@ecohealthalliance.org>
2021-04-27 17:06:00 -0400

    Tweaks to make data push/pull smoother
    Use 0.0.0.0 instead of url to be machine-agnostic
    Overwrite dump file

--- scraper/pull_db_aws.sh
+++ scraper/pull_db_aws.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+echo #!/bin/bash
 
 set -e
 set -x
@@ -12,7 +12,7 @@ export PGDATABASE=$POSTGRES_DB
 # pulls current AWS backup and replaces local repel database with it.
 
 aws s3 cp s3://${AWS_BUCKET}/dumps/${PGDUMP_FILENAME}.xz repel_backup.dmp.xz
-unxz repel_backup.dmp.xz
+unxz --force repel_backup.dmp.xz
 dropdb repel || { echo "Warning: failed to drop repel database!"; }
 createdb repel || { echo "Error: failed to create repel database!" && exit 1; }
 psql repel < repel_backup.dmp || { echo "Error: failed to restore repel database from backup!" && exit 1; }
