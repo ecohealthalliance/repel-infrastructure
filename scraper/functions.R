54f1ad74c744ae615c9dfdfff4e756e01557cee0
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-05-06 13:57:36 -0400

    refactoring wahis_db_connect

--- scraper/functions.R
+++ scraper/functions.R
@@ -1,41 +1,37 @@
 `%||%` <- function(a, b) if (is.null(a)) return(b) else return(a)
 
 # Connect to WAHIS Database
-wahis_db_connect <- function(try_local = TRUE){
+wahis_db_connect <- function(host_location = c("reservoir", "local", "remote")){
+
   # read env file
   env_file <- stringr::str_remove(here::here(".env"), "scraper/")
   base::readRenviron(env_file)
 
+  # assign host name and port
+  host_location <- match.arg(host_location)
+  host_name <- switch(host_location,
+                      "local" = "0.0.0.0",
+                      "reservoir" =  paste0( stringr::str_extract( Sys.info()["nodename"], "aegypti|prospero"), ".ecohealthalliance.org"),
+                      "remote" = Sys.getenv("POSTGRES_HOST"))
+  port <- switch(host_location,
+                 "local" = "22053",
+                 "reservoir" =  "22053",
+                 "remote" = Sys.getenv("POSTGRES_PORT"))
 
-  # Try a local host, otherwise connect to production
-  if(try_local) {
-    message("Attempting to connect to local REPEL db at 0.0.0.0:22053")
-    conn <- try(dbConnect(
-      RPostgres::Postgres(),
-      host = "0.0.0.0",
-      port = "22053",
-      user = Sys.getenv("POSTGRES_USER"),
-      password = Sys.getenv("POSTGRES_PASSWORD"),
-      dbname = Sys.getenv("POSTGRES_DB")
-    ), silent = FALSE)
-  }
-
-  if(inherits(conn, "try-error")) {
-    message(glue::glue('Attempting to connect to REPEL db at {Sys.getenv("POSTGRES_HOST")}:{Sys.getenv("POSTGRES_PORT")}'))
-    conn <- dbConnect(
-      RPostgres::Postgres(),
-      host = Sys.getenv("POSTGRES_HOST"),
-      port = Sys.getenv("POSTGRES_PORT"),
-      user = Sys.getenv("POSTGRES_USER"),
-      password = Sys.getenv("POSTGRES_PASSWORD"),
-      dbname = Sys.getenv("POSTGRES_DB")
-    ), silent = TRUE)
-  }
-
+  message(glue::glue('Attempting to connect to REPEL db at {host_name}:{port}'))
+  conn <- dbConnect(
+    RPostgres::Postgres(),
+    host = host_name,
+    port = port,
+    user = Sys.getenv("POSTGRES_USER"),
+    password = Sys.getenv("POSTGRES_PASSWORD"),
+    dbname = Sys.getenv("POSTGRES_DB")
+  )
 
   if (require("connections")) {
     connections::connection_view(conn, name = "repel", connection_code = "repel")
   }
+
   info <- dbGetInfo(conn)
   message(glue::glue("Connected to database \"{info$dbname}\" at {info$host}:{info$port}"))
   return(conn)
