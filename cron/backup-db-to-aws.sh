90864fc8a7a6643149481b1a6f5963b63a72982d
Rob Young <young@ecohealthalliance.org>
2021-06-15 13:05:52 -0400

    updated backup scripts with option for production bucket

--- cron/backup-db-to-aws.sh
+++ cron/backup-db-to-aws.sh
@@ -10,7 +10,9 @@ export PGDATABASE=$POSTGRES_DB
 
 # Backup the whole DB to S3
 if [ "$BACKUP_PG" == "1" ]; then
+  if []
   echo "Dumping $PGDATABASE and archiving on S3 bucket $AWS_BUCKET"
-  pg_dumpall | xz -9 -c |\
-    aws s3 cp - s3://${AWS_BUCKET}/dumps/${PGDUMP_FILENAME}.xz
+  pg_dumpall | xz -9 -c > /tmp/tmp.sql.xz
+  aws s3 cp /tmp/tmp.sql.xz s3://${AWS_BUCKET}/dumps/${PGDUMP_FILENAME}.xz
+  rm /tmp/tmp.sql.xz
 fi
--- cron/export-csv-to-aws.sh
+++ cron/export-csv-to-aws.sh
@@ -8,19 +8,26 @@ export PGPORT=$POSTGRES_PORT
 export PGHOST=$POSTGRES_HOST
 export PGDATABASE=$POSTGRES_DB
 
+if [ "$IS_PROD" == "yes" ]
+then
+  target_bucket=${AWS_BUCKET_PROD}
+else
+  target_bucket=${AWS_BUCKET}
+fi
+
 # Backup the whole DB to S3
 if [ "$BACKUP_CSV" == "1" ]; then
-  echo "Exporting CSVs from $PGDATABASE and archiving on S3 bucket $AWS_BUCKET"
+  echo "Exporting CSVs from $PGDATABASE and archiving on S3 bucket $target_bucket"
 
 # remove existing csv files before copying new ones
-aws s3 rm s3://${AWS_BUCKET}/csv --recursive
+aws s3 rm s3://${target_bucket}/csv --recursive
 
 # The `sem` command allows up to `j` jobs to be run in the background,
 # Then `sem --wait` pauses execution until they are all done.  Doing this to
 # allow parallel, but limited, connections to the database.
 psql -Atc "select tablename from pg_tables where schemaname='public'" |\
   while read TBL; do
-    sem -j 10 "psql -c \"COPY $TBL TO STDOUT WITH NULL AS 'NA' CSV HEADER\" | xz -9 -c | aws s3 cp - s3://${AWS_BUCKET}/csv/${TBL}.csv.xz --acl public-read"
+    sem -j 10 "psql -c \"COPY $TBL TO STDOUT WITH NULL AS 'NA' CSV HEADER\" | xz -9 -c | aws s3 cp - s3://${target_bucket}/csv/${TBL}.csv.xz --acl public-read"
   done
 fi
 sem --wait
