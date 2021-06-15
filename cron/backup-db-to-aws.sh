45fabcb5b2c7e6c00507ddd264ebd6b346b62f3a
Rob Young <young@ecohealthalliance.org>
2021-06-15 13:24:23 -0400

    start cron with aws deploy and test backup script

--- cron/backup-db-to-aws.sh
+++ cron/backup-db-to-aws.sh
@@ -10,9 +10,14 @@ export PGDATABASE=$POSTGRES_DB
 
 # Backup the whole DB to S3
 if [ "$BACKUP_PG" == "1" ]; then
-  if []
-  echo "Dumping $PGDATABASE and archiving on S3 bucket $AWS_BUCKET"
-  pg_dumpall | xz -9 -c > /tmp/tmp.sql.xz
-  aws s3 cp /tmp/tmp.sql.xz s3://${AWS_BUCKET}/dumps/${PGDUMP_FILENAME}.xz
-  rm /tmp/tmp.sql.xz
+  if [ "$IS_PROD" == "yes" ]
+  then
+    target_bucket=${AWS_BUCKET_PROD}
+  else
+    target_bucket=${AWS_BUCKET}
+  fi
+  echo "Dumping $PGDATABASE and archiving on S3 bucket $target_bucket"
+#  pg_dumpall | xz -9 -c > /tmp/tmp.sql.xz
+#  aws s3 cp /tmp/tmp.sql.xz s3://${target_bucket}/dumps/${PGDUMP_FILENAME}.xz
+#  rm /tmp/tmp.sql.xz
 fi
