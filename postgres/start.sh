cae392dec871aa00cffe6dbbc169a3a9979bd6dd
Rob Young <young@ecohealthalliance.org>
2021-06-18 15:30:36 -0400

    add separate cron container back
    multi service container may not be much of a savings.

--- postgres/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PGUSER=$POSTGRES_USER
-export PGPASSWORD=$POSTGRES_PASSWORD
-export PGPORT=$POSTGRES_PORT
-export PGHOST=$POSTGRES_HOST
-export PGDATABASE=$POSTGRES_DB
-
-/usr/local/bin/supercronic backup-schedule.cron >> /var/log/shared/backups.log 2>&1
