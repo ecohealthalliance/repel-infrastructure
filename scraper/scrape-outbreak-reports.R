2dc4282b438c9eb726960922ca4da3f571a9bf35
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-07-28 16:12:40 -0400

    workflow that runs full outbreak model prediction cache following scraper. removes separate cache prediction script.

--- scraper/scrape-outbreak-reports.R
+++ scraper/scrape-outbreak-reports.R
@@ -3,6 +3,7 @@
 dir <- ifelse(basename(getwd())=="repel-infrastructure", "scraper/", "")
 source(here::here(paste0(dir, "packages.R")))
 source(here::here(paste0(dir, "functions.R")))
+library(repelpredict)
 
 # Connect to database ----------------------------
 message("Connect to database")
@@ -73,7 +74,7 @@ if(any(!unique(outbreak_reports_ingest_status_log$ingest_error))){ # check if th
   outbreak_report_tables <- transpose(outbreak_report_tables) %>%
     map(function(x) reduce(x, bind_rows))
 
-  outbreak_report_tables$outbreak_reports_diseases_unmatched <- distinct(outbreak_report_tables$outbreak_reports_diseases_unmatched)
+  outbreak_report_tables$outbreak_reports_diseases_unmatched <- outbreak_report_tables$outbreak_reports_diseases_unmatched %>% unique()
 
   outbreak_report_tables$outbreak_reports_ingest_status_log <- outbreak_reports_ingest_status_log
 
@@ -99,7 +100,7 @@ if(any(!unique(outbreak_reports_ingest_status_log$ingest_error))){ # check if th
           })
   }
 
-  message("Done updating outbreak reports.")
+  message("Done updating outbreak reports")
 
   # Schema lookup -----------------------------------------------------------
   # field_check(conn, "outbreak_reports_")
@@ -114,6 +115,70 @@ if(any(!unique(outbreak_reports_ingest_status_log$ingest_error))){ # check if th
   #   output_file = paste0("outbreak-report-qa.html"),
   #   output_dir = here::here(paste0(dir, "reports"))
   # )
+
+  # Update model predictions with new data------------------------------------------------
+  message("Updating cached model predictions")
+
+  # get model from aws
+  model_object <-  repelpredict::network_lme_model(
+    network_model = aws.s3::s3readRDS(bucket = "repeldb/models", object = "lme_mod_network.rds"),
+    network_scaling_values = aws.s3::s3readRDS(bucket = "repeldb/models", object = "network_scaling_values.rds")
+  )
+
+  aws_network_etag <- aws.s3::head_object(bucket = "repeldb/models", object = "lme_mod_network.rds") %>%
+    attr(., "etag")
+
+  # get full database
+  repeldat <- repelpredict::repel_split(model_object, conn)
+
+  # forecast combines augment and predict
+  repel_forecast_out <- repel_forecast(model_object = model_object,
+                                       conn = conn,
+                                       newdata = repeldat,
+                                       use_cache = FALSE)
+
+  network_lme_augment_predict <- repel_forecast_out[[1]] %>%
+    mutate(predicted_outbreak_probability = repel_forecast_out[[2]]) %>%
+    mutate(db_network_etag = aws_network_etag)
+
+  forecasted_predictions <- network_lme_augment_predict %>%
+    distinct(country_iso3c, disease, month, predicted_outbreak_probability)
+
+  # write_rds(network_lme_augment_predict, "tmp_forecasted_data.rds")
+  # network_lme_augment_predict <- read_rds("tmp_forecasted_data.rds")
+  dbWriteTable(conn, name = "network_lme_augment_predict", network_lme_augment_predict, overwrite = TRUE)
+
+  ### cache augment with disaggregated country imports
+  augmented_data_disagg <- repel_augment(model_object, conn, newdata = network_lme_augment_predict, sum_country_imports = FALSE)
+
+  network_lme_augment_predict_by_origin <- augmented_data_disagg %>%
+    drop_na(country_origin) %>%
+    left_join(forecasted_predictions)
+
+  dbWriteTable(conn, name = "network_lme_augment_predict_by_origin", network_lme_augment_predict_by_origin, overwrite = TRUE)
+
+  ### cache model coefficients (not necessary every time there is new data, but doesn't hurt to override to ensure everything is current)
+  lme_mod <- model_object$network_model
+
+  randef <- lme4::ranef(lme_mod)
+  randef_disease <- randef$disease %>%
+    tibble::rownames_to_column(var = "disease") %>%
+    as_tibble() %>%
+    pivot_longer(-disease, names_to = "variable", values_to = "coef") %>%
+    mutate(disease_clean = str_to_title(str_replace_all(disease, "_", " "))) %>%
+    mutate(variable_clean = str_replace(variable, "_from_outbreaks", " from countries with existing outbreak"),
+           variable_clean = str_replace(variable_clean, "fao_trade_", ""),
+           variable_clean = str_replace(variable_clean, "_other", " (other)"),
+           variable_clean = str_replace_all(variable_clean, "_", " "),
+           variable_clean = str_remove(variable_clean, "continent"),
+           variable_clean = str_replace(variable_clean, "shared borders from countries with existing outbreak", "shared borders with country with existing outbreak"))
+
+  dbWriteTable(conn, name = "network_lme_coefficients", randef_disease, overwrite = TRUE)
+
+  network_scaling_values <- model_object$network_scaling_values
+  dbWriteTable(conn, name = "network_lme_scaling_values", network_scaling_values, overwrite = TRUE)
+
+  #TODO try alternative which is just predicting on new subset of the data. use init/split to preformat, then predict
 
 }
 
