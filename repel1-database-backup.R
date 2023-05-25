de093261d0712b26b969e8afe47e5a39d5a26c9b
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2023-05-25 11:09:12 -0400

    script to backup repel 1 database

---
+++ repel1-database-backup.R
@@ -0,0 +1,10 @@
+source(here::here("scraper", "packages.R"))
+conn <- repeldata::repel_remote_conn()
+
+tbls_to_backup <- dbListTables(conn)
+tbls_to_backup <- tbls_to_backup[!str_starts(tbls_to_backup, "raster_")]
+
+for(tb in tbls_to_backup){
+  tmp <- dbReadTable(conn, tb)
+  arrow::write_parquet(tmp, glue::glue("repel1_database_backup/{tb}.gz.parquet"))
+}
